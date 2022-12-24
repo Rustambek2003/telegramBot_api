e08494991f971d90282e005f15c046a607b3208d
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-12-24 16:05:18 +0500

    'sendmessages'

--- sendMessage.py
+++ sendMessage.py
@@ -1,4 +1,15 @@
 import requests
 def sendMssage(chat_id,text):
-    pass
+    TOKEN = '5766174948:AAFPOJO7Udzs9sAwjnb67LBjsNmubxIm5rQ'
+    BASE_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+    param = {
+        'chat_id':chat_id,
+        'text':text
+    }
+
+    data = requests.get(BASE_URL, param)
+    return data.json()
+
+print(sendMssage(996172963,'Salom'))
 
