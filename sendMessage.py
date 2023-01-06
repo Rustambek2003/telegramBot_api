21300b842b118fe27e09c40fb8591c0d1b60b999
Rustambek2003 <rustambekjumaqulov@gmail.com>
2023-01-06 11:22:09 +0500

    'message'

--- sendMessage.py
+++ sendMessage.py
@@ -1,15 +1,20 @@
-import requests
-def sendMssage(chat_id,text):
-    TOKEN = '5766174948:AAFPOJO7Udzs9sAwjnb67LBjsNmubxIm5rQ'
-    BASE_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+# import requests
+# def sendMssage(chat_id,photo):
+#     TOKEN = '5766174948:AAFPOJO7Udzs9sAwjnb67LBjsNmubxIm5rQ'
+#     BASE_URL = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
 
-    param = {
-        'chat_id':chat_id,
-        'text':text
-    }
+#     param = {
+#         'chat_id':chat_id,
+#         'photo':photo
+#     }
+#     files = {
+#         'photo':photo
+#     }
 
-    data = requests.get(BASE_URL, param)
-    return data.json()
-
-print(sendMssage(996172963,'Salom'))
+#     data = requests.get(url=BASE_URL, params=param)
+#     print(data.json())
+# # photo = 'https://random.dog/15038-13875-14202.jpg'
+# photo = "AgACAgIAAxkBAAMnY7ezjNMhfqkXJVOM8XxpgB8LoSEAAu3BMRvK3LlJTc69b5lsz8EBAAMCAANzAAMtBA"
+# imj = open('logo.jpg','rb').read()
+# print(sendMssage(996172963, photo))
 
