609d25e813bfce2148b7fa13d337a715d01ab0e4
Rustambek2003 <rustambekjumaqulov@gmail.com>
2023-01-07 18:02:08 +0500

    keyboard

---
+++ sendKeyboard.py
@@ -0,0 +1,33 @@
+# import os 
+# import requests
+
+# TOKEN = os.environ['TOKEN']
+
+# def sendMessage(chat_id:str,text:str):
+#     URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+#     button = [
+#     {
+#         'text':'button'
+#     },
+#     {
+#         'text':'21'
+#     },
+#     {
+#         'text':'Home'
+#     }
+#     ]
+#     keyboard =  [[button[0]],[button[0]]]
+#     reply_markup = {
+#         'keyboard':keyboard
+#     }
+
+#     payload = {
+#         'chat_id':chat_id,
+#         'text':'Salom',
+#         'reply_markup':reply_markup
+#     }
+#     r = requests.get(url=URL,json=payload)
+
+# chat_id ='996172963'
+
+# sendMessage(chat_id=chat_id,text='Salom')
